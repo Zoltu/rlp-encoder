48c9a747b36be6846f9017dc3845e715494eead4
Micah Zoltu <micah@zoltu.net>
2019-06-27 01:57:00 +0800

    Removes utils library because it breaks ts-node https://github.com/TypeStrong/ts-node/issues/783.
    Unfortunately, at the moment you have to choose whether to write imports that work in ts-node, or that work in the browser as es-modules.  Luckily, for this project there was one function in a second file so we can just pull that into the index file and circumvent the problem.  For other projects, resolving this issue will get a lot more messy.

--- source/index.ts
+++ source/index.ts
@@ -1,5 +1,3 @@
-import { hexStringToUint8Array } from './utils'
-
 export type RlpItem = Uint8Array | RlpItemArray
 export interface RlpItemArray extends Array<RlpItem> {}
 
@@ -68,4 +66,17 @@ function rlpEncodeLarge(data: Uint8Array): Uint8Array {
 	result.set(lengthBytes, 1)
 	result.set(data, 1 + lengthBytes.length)
 	return result
+}
+
+function hexStringToUint8Array(hex: string): Uint8Array {
+	const match = new RegExp(`^(?:0x)?([a-fA-F0-9]*)$`).exec(hex)
+	if (match === null) throw new Error(`Expected a hex string encoded byte array with an optional '0x' prefix but received ${hex}`)
+	const maybeLeadingZero = (match[1].length % 2) ? '0' : ''
+	const normalized = `${maybeLeadingZero}${match[1]}`
+	const byteLength = normalized.length / 2
+	const bytes = new Uint8Array(byteLength)
+	for (let i = 0; i < byteLength; ++i) {
+		bytes[i] = (Number.parseInt(`${normalized[i*2]}${normalized[i*2+1]}`, 16))
+	}
+	return bytes
 }
